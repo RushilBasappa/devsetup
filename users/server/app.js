85b1dd5ef1dad70a9648f25fe89f46a663acd18f
Rushil Basappa <rushil.oucse37@gmail.com>
2020-05-06 10:26:49 +0530

    add cors support

--- users/server/app.js
+++ users/server/app.js
@@ -8,6 +8,12 @@ import indexRouter from "./routes/index";
 import usersRouter from "./routes/users";
 
 const app = express();
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
 app.use(logger("dev"));
 app.use(express.json());
