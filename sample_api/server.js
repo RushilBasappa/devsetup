82b8bac3233fedec2e0f68e17b3e22b879c05d2d
Rushil Basappa <rushil.oucse37@gmail.com>
2020-05-12 19:41:59 +0530

    updated service

--- sample_api/server.js
+++ sample_api/server.js
@@ -17,8 +17,17 @@ const users = {
   },
 };
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+
 // App
 const app = express();
+
+app.use(allowCrossDomain);
 
 app.get('/', (req, res) => {
   res.send(Object.values(users));
